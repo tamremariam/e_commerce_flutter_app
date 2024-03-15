eb585375a9a35c2b8e46bd0414ab02a615c32d54
Tamr <ethiotamremariam@gmail.com>
2024-03-15 15:09:38 +0300

    fixing utility

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,14 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  // runApp(const App());
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
 }
